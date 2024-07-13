e3818bab4bf1c360b83ef0b58d305ac26635dbcc
shigaka <shigaka@istyle.co.jp>
2024-07-13 17:02:33 +0900

    OIDC修正

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
-output "oidc_provider_arn" {
-  description = "The ARN of the OIDC provider"
-  value       = data.aws_iam_openid_connect_provider.github.arn
+output "github_actions_role_arn" {
+  description = "The ARN of the GitHub Actions IAM role"
+  value       = aws_iam_role.github_actions_role.arn
 }
 
 data "aws_iam_openid_connect_provider" "github" {
